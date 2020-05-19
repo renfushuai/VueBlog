9d95e52594e1ec85c0178d956737a47f175505dc
王敬玉 <wangjingyu@192.168.1.3>
2020-05-19 22:11:36 +0800

    first commit

---
+++ src/views/Welcome.vue
@@ -0,0 +1,25 @@
+<template>
+          <div style="margin: 20px;"> 欢迎来到 BlogAdmin 后台管理系统!</div>
+</template>
+
+<script>
+    export default {
+        name: "Welcome",
+        mounted() {
+            var curTime = new Date()
+            if(window.localStorage.TokenExpire){
+                var expiretime = new Date(Date.parse(window.localStorage.TokenExpire))
+                if(curTime>=expiretime){
+                    this.$router.push('/login');
+                }
+            }else {
+                this.$router.push('/login');
+            }
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
